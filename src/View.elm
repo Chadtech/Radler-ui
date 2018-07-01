2b4e5f2aa1cb252713541cea8c1ba3bf430ae6d6
Chadtech <chadstearns1@gmail.com>
2018-07-01 14:47:02 +0200

    There is now a header and a body, and scrolling the body is independent of the header

--- src/View.elm
+++ src/View.elm
@@ -2,11 +2,17 @@ module View exposing (view)
 
 import Array
 import Browser
+import Css exposing (..)
 import Data.Tracker exposing (Tracker)
-import Html exposing (Html, div)
-import Html.Styled
+import Html.Grid as Grid
+import Html.Styled as Html exposing (Html, div)
+import Html.Styled.Attributes as Attrs
+    exposing
+        ( css
+        )
 import Model exposing (Model)
 import Msg exposing (Msg(..))
+import Style
 import Tracker.View as Tracker
 
 
@@ -17,15 +23,53 @@ view : Model -> Browser.Document Msg
 view model =
     { title = "Radler"
     , body =
-        model.trackers
-            |> Array.toIndexedList
-            |> List.map (viewTracker model)
+        [ header
+        , bodyView model
+        ]
+            |> List.map Html.toUnstyled
     }
+
+
+header : Html Msg
+header =
+    Grid.row
+        []
+        [ Grid.column
+            []
+            [ div
+                [ css
+                    [ Style.card
+                    , displayFlex
+                    , height (px 16)
+                    ]
+                ]
+                []
+            ]
+        ]
+
+
+bodyView : Model -> Html Msg
+bodyView model =
+    Grid.row
+        []
+        [ Grid.column
+            [ css [ overflow auto ] ]
+            [ Grid.container
+                [ css [ display inlineFlex ] ]
+                (viewTrackers model)
+            ]
+        ]
+
+
+viewTrackers : Model -> List (Html Msg)
+viewTrackers model =
+    model.trackers
+        |> Array.toIndexedList
+        |> List.map (viewTracker model)
 
 
 viewTracker : Model -> ( Int, ( Int, Tracker ) ) -> Html Msg
 viewTracker model ( index, rest ) =
     ( index, rest )
         |> Tracker.view model
-        |> Html.Styled.toUnstyled
         |> Html.map (TrackerMsg index)
